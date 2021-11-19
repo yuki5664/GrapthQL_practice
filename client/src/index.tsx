2d03ce23246e2276cb7441d559a8ac3479eb93f7
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-11-19 10:38:33 +0900

    Add implement login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,50 @@
 import {
   ApolloClient,
   NormalizedCacheObject,
-  ApolloProvider
+  ApolloProvider,
+  gql,
+  useQuery
 } from '@apollo/client';
+import Login from './pages/login';
 import { cache } from './cache';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
 import injectStyles from './styles';
 
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+`;
 // Initialize ApolloClient
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   uri: 'http://localhost:4000/graphql',
+  headers: {
+    authorization: localStorage.getItem('token') || '',
+  },
+  typeDefs,
 });
 
 injectStyles();
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 // Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById('root')
 );
